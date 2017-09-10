a010f7dd447bf647e2f07c9f3a7b8193a38ed591
Dustin Grady <dgrady@csumb.edu>
2017-09-10 09:24:54 +0000

    Updated 777 project

--- 777/functions.php
+++ 777/functions.php
@@ -1,11 +1,11 @@
 <?php
 
     function play(){
-        for($i=1; $i<4; $i++){
-            ${"randomValue" . $i } = rand(0,2);
+        for($i=1; $i<5; $i++){
+            ${"randomValue" . $i } = rand(0,3);
             displaySymbol(${"randomValue" . $i}, $i );
         }
-        displayPoints($randomValue1, $randomValue2, $randomValue3);
+        displayPoints($randomValue1, $randomValue2, $randomValue3, $randomvalue4);
     }
     
         
@@ -17,20 +17,23 @@
                 break;
             case 2: $symbol = "lemon";
                 break;
+            case 3: $symbol = "bitcoin";
             }
             echo "<img id='reel$pos' src='img/$symbol.png' alt='$symbol' title='".ucfirst($symbol) . "' width='70' >";
         }
         
-    function displayPoints($randomValue1, $randomValue2, $randomValue3){
+    function displayPoints($randomValue1, $randomValue2, $randomValue3, $randomValue4){
         echo "<div id='output'>";
-        if($randomValue1 == $randomValue2 && $randomValue2 == $randomValue3){
+        if($randomValue1 == $randomValue2 && $randomValue2 == $randomValue3 && $randomvalue3 == $randomValue4){
             switch($randomValue1){
                 case 0: $totalPoints = 1000;
                     echo "<h1>Jackpot!</h1>";
                     break;
-                case 1: $totalPoints = 500;
+                case 1: $totalPoints = 900;
                     break;
-                case 2: $totalPoints = 250;
+                case 2: $totalPoints = 500;
+                    break;
+                case 3: $totalPoints = 250;
                     break;
             }
             echo "<h2>You won $totalPoints points!</h2>";
