f5421e3ae16521ca1eb3c8828f81497e0d9c6743
Dustin Grady <dgrady@csumb.edu>
2017-09-10 08:56:32 +0000

    Added 777 project

---
+++ 777/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <style>
+        @import url("css/styles.css");
+    </style>
+    <head>
+        <title> 777 Slot Machine </title>
+    </head>
+
+    <div id="main"></div>
+        <?php
+        include './functions.php';
+        
+        play();
+        
+        ?>
+        
+        <form>
+            <input type="submit" value ="Spin!"/>
+        </form>
+    </div>
+    
+</html>
