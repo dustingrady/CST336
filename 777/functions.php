7ba2718fde395c36f7fce87ae082a026b78e7423
Dustin Grady <dgrady@csumb.edu>
2017-09-12 21:25:46 +0000

    Completed 777 project

--- 777/functions.php
+++ 777/functions.php
@@ -1,30 +1,30 @@
 <?php
 
     function play(){
-        for($i=1; $i<5; $i++){
+        for($i=1; $i<4; $i++){
             ${"randomValue" . $i } = rand(0,3);
             displaySymbol(${"randomValue" . $i}, $i );
         }
-        displayPoints($randomValue1, $randomValue2, $randomValue3, $randomvalue4);
+        displayPoints($randomValue1, $randomValue2, $randomValue3);
     }
     
-        
     function displaySymbol($randomValue, $pos){
         switch($randomValue){
             case 0: $symbol = "seven";
                 break;
-            case 1: $symbol = "cherry";
+            case 1: $symbol = "bitcoin";
                 break;
-            case 2: $symbol = "lemon";
+            case 2: $symbol = "cherry";
                 break;
-            case 3: $symbol = "bitcoin";
+            case 3: $symbol = "lemon";
+                break;
             }
             echo "<img id='reel$pos' src='img/$symbol.png' alt='$symbol' title='".ucfirst($symbol) . "' width='70' >";
         }
         
-    function displayPoints($randomValue1, $randomValue2, $randomValue3, $randomValue4){
+    function displayPoints($randomValue1, $randomValue2, $randomValue3){
         echo "<div id='output'>";
-        if($randomValue1 == $randomValue2 && $randomValue2 == $randomValue3 && $randomvalue3 == $randomValue4){
+        if($randomValue1 == $randomValue2 && $randomValue2 == $randomValue3){
             switch($randomValue1){
                 case 0: $totalPoints = 1000;
                     echo "<h1>Jackpot!</h1>";
